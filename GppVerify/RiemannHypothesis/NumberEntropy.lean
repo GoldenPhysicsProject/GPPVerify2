b7dc2270be8f8289f05f2d88f75ef7ec5d6b5f66
GoldenPhysicsProject <dtoupin@goldenphysics.org>
2026-08-24 07:01:40 -0400

    Wire finite weighted variance positivity into number entropy

--- GppVerify/RiemannHypothesis/NumberEntropy.lean
+++ GppVerify/RiemannHypothesis/NumberEntropy.lean
@@ -1,5 +1,6 @@
 import GppVerify.RiemannHypothesis.GlobalVonMangoldtBridge
 import GppVerify.RiemannHypothesis.PrimeGasPartition
+import GppVerify.RiemannHypothesis.WeightedVarianceFinite
 import Mathlib.Tactic
 
 /-!
