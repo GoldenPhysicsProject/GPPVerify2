3cfb3d0937a21845350cb06f69f30424c308d8cd
GoldenPhysicsProject <dtoupin@goldenphysics.org>
2026-08-24 07:19:33 -0400

    Wire reconstructed zeta moment hierarchy into number entropy

--- GppVerify/RiemannHypothesis/NumberEntropy.lean
+++ GppVerify/RiemannHypothesis/NumberEntropy.lean
@@ -1,6 +1,6 @@
 import GppVerify.RiemannHypothesis.GlobalVonMangoldtBridge
 import GppVerify.RiemannHypothesis.PrimeGasPartition
-import GppVerify.RiemannHypothesis.ZetaGibbsSummability
+import GppVerify.RiemannHypothesis.ZetaGibbsMoments
 import Mathlib.Tactic
 
 /-!
