e18ca1f6a2baa621e0aa3fe00e8c16b40947c0e0
Daniel Toupin <dtoupin@goldenphysics.org>
2026-09-10 06:23:14 -0400

    repair finite odd-log derivative Finset binder

--- GppVerify/QuantumGravity/OddLatticeDerivativeUniform.lean
+++ GppVerify/QuantumGravity/OddLatticeDerivativeUniform.lean
@@ -59,9 +59,8 @@ theorem hasDerivAt_oddLogPartialSum (N : ℕ) (x : ℝ) :
     HasDerivAt
       (fun y : ℝ => ∑ n in Finset.range N, oddLogTerm n y)
       (∑ n in Finset.range N, oddDerivativeTerm n x) x := by
-  simpa only [Finset.sum_apply] using
-    (HasDerivAt.fun_sum (s := Finset.range N)
-      (fun n _ => hasDerivAt_oddLogTerm n x))
+  exact HasDerivAt.fun_sum (u := Finset.range N)
+    (fun n _ => hasDerivAt_oddLogTerm n x)
 
 /-- The standard p-series majorant used on `[-T,T]`. -/
 def oddDerivativeMajorant (T : ℝ) (n : ℕ) : ℝ :=
